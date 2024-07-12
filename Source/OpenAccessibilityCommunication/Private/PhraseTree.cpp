9e50b30b949966a6eed87a7de4447e07dd79c7c2
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2024-07-12 10:43:46 +0100

    Phrase Chaining (#35)
    Quick Implementation of Phrase Chaining, will be refactored with a more
    scalable approach on the whole of the propagation pipeline.
    
    Allows for chaining by repropagating on the tree upon success or
    failure, with failure removing the failed upon command.

--- Source/OpenAccessibilityCommunication/Private/PhraseTree.cpp
+++ Source/OpenAccessibilityCommunication/Private/PhraseTree.cpp
@@ -38,25 +38,8 @@ void FPhraseTree::ParseTranscription(TArray<FString> InTranscriptionSegments)
 	}
 
 	TArray<FString> SegmentWordArray = TArray<FString>();
-	const TCHAR* ParseDelimsArray[11] = {
-		// Whitespace Delims
-		TEXT(" "),
-		TEXT("\t"),
-		TEXT("\r"),
-		TEXT("\n"),
-		TEXT(""),
-
-		// Punctuation Delims
-		TEXT("."),
-		TEXT(","),
-		TEXT("!"),
-		TEXT("?"),
-		TEXT(":"),
-		TEXT(";"),
-	};
-	int ParseDelimsCount = UE_ARRAY_COUNT(ParseDelimsArray);
-
 	int SegmentCount = 0;
+
 	for (FString& TranscriptionSegment : InTranscriptionSegments)
 	{
 		if (TranscriptionSegment.IsEmpty())
@@ -83,45 +66,64 @@ void FPhraseTree::ParseTranscription(TArray<FString> InTranscriptionSegments)
 
 		Algo::Reverse(SegmentWordArray);
 
-		FParseRecord ParseRecord = FParseRecord(ContextManager.GetContextStack());
-		FParseResult ParseResult = ParsePhrase(SegmentWordArray, ParseRecord);
-
-		ContextManager.UpdateContextStack(ParseRecord.ContextObjectStack);
-
-		UE_LOGFMT(LogOpenAccessibilityCom, Log, "|| Phrase Tree || Segment: {0} | Result: {1} ||", SegmentCount, ParseResult.Result);
-
-		switch (ParseResult.Result)
+		while (!SegmentWordArray.IsEmpty())
 		{
-			case PHRASE_PARSED:
-			case PHRASE_PARSED_AND_EXECUTED:
+
+			FParseRecord ParseRecord = FParseRecord(ContextManager.GetContextStack());
+			FParseResult ParseResult = ParsePhrase(SegmentWordArray, ParseRecord);
+
+			ContextManager.UpdateContextStack(ParseRecord.ContextObjectStack);
+
+			UE_LOGFMT(LogOpenAccessibilityCom, Log, "|| Phrase Tree || Segment: {0} | Result: {1} ||", SegmentCount, ParseResult.Result);
+
+			switch (ParseResult.Result)
 			{
-				OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Success} Phrase Tree Parsed Correctly (%s)"),
-					*ParseRecord.GetPhraseString())
-				
-				LastVistedNode.Reset();
-				LastVistedParseRecord = FParseRecord();
+				case PHRASE_PARSED:
+				case PHRASE_PARSED_AND_EXECUTED:
+				{
+					OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Success} Phrase Tree Parsed Correctly (%s)"),
+						*ParseRecord.GetPhraseString())
 
-				break;
-			}
+					LastVistedNode.Reset();
+					LastVistedParseRecord = FParseRecord();
 
-			case PHRASE_REQUIRES_MORE:
-			case PHRASE_REQUIRES_MORE_CORRECT_PHRASES:
-			{
-				OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Failed} Phrase Tree Propagation Requires More Segments. (%s)"),
-					*ParseRecord.GetPhraseString())
+					break;
+				}
 
-				LastVistedNode = ParseResult.ReachedNode;
-				LastVistedParseRecord = ParseRecord;
+				case PHRASE_REQUIRES_MORE:
+				{
+					OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Failed} Phrase Tree Propagation Requires More Segments. (%s)"), 
+						*ParseRecord.GetPhraseString());
 
-				break;
-			}
+					LastVistedNode = ParseResult.ReachedNode;
+					LastVistedParseRecord = ParseRecord;
+				}
 
-			case PHRASE_UNABLE_TO_PARSE:
-			{
-				OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Failed} Phrase Tree Propagation Failed. (%s)"),
-					*ParseRecord.GetPhraseString())
+				case PHRASE_REQUIRES_MORE_CORRECT_PHRASES:
+				{
+					OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Failed} Phrase Tree Propagation Requires More Correct Segments. (%s)"),
+						*ParseRecord.GetPhraseString())
 
-				break;
+					LastVistedNode = ParseResult.ReachedNode;
+					LastVistedParseRecord = ParseRecord;
+
+					if (!SegmentWordArray.IsEmpty())  
+						SegmentWordArray.Pop();
+
+					break;
+				}
+
+				default:
+				case PHRASE_UNABLE_TO_PARSE:
+				{
+					OA_LOG(LogOpenAccessibilityCom, Log, TEXT("PhraseTree Propagation"), TEXT("{Failed} Phrase Tree Propagation Failed. (%s)"),
+						*ParseRecord.GetPhraseString())
+
+					if (!SegmentWordArray.IsEmpty())
+						SegmentWordArray.Pop();
+
+					break;
+				}
 			}
 		}
 
@@ -143,7 +145,7 @@ FParseResult FPhraseTree::ParsePhrase(TArray<FString>& InPhraseWordArray, FParse
 	// due to the possibility of connecting phrases over different transcription segments.
 	if (LastVistedNode != nullptr && LastVistedNode.IsValid())
 	{
-		TArray<FString> PhraseWordArrayCopy = TArray<FString>(InPhraseWordArray);
+		TArray<FString> PhraseWordArrayCopy = TArray(InPhraseWordArray);
 
 		FParseResult ParseResult = LastVistedNode->ParseChildren(PhraseWordArrayCopy, LastVistedParseRecord);
 		if (ParseResult.Result == PHRASE_PARSED)
@@ -160,22 +162,15 @@ FParseResult FPhraseTree::ParsePhrase(TArray<FString>& InPhraseWordArray, FParse
 		}
 	}
 
-	// Check if the Context Stack has Objects, if so propogate from the Context Root.
-	// Otherwise, start a new propogation entirely from the Tree Root.
+	// Check if the Context Stack has Objects, if so propagation from the Context Root.
 	if (ContextManager.HasContextObjects())
 	{
-		// Propogate from the Context Root, that is the Top of the Context Stack.
-
+		// Propagate from the Context Root, that is the Top of the Context Stack.
 		return ContextManager.PeekContextObject()->GetContextRoot()->ParsePhraseAsContext(InPhraseWordArray, InParseRecord);
 	}
-	else
-	{
-		return ParseChildren(InPhraseWordArray, InParseRecord);
-	}
 
-	UE_LOG(LogOpenAccessibilityCom, Warning, TEXT("|| Phrase Tree || No Parse Path Found ||"));
-
-	return FParseResult(PHRASE_NOT_PARSED);
+	// Otherwise, start a new propagation entirely from the Tree Root.
+	return ParseChildren(InPhraseWordArray, InParseRecord);
 }
 
 void FPhraseTree::BindBranch(const TPhraseNode& InNode)
@@ -204,7 +199,7 @@ void FPhraseTree::BindBranch(const TPhraseNode& InNode)
 			}
 		}
 
-		// If the Start Node has no similar chilren, then bind the branch to the start node.
+		// If the Start Node has no similar children, then bind the branch to the start node.
 		// Can force bind, as previous checks show no child is similar.
 		BranchToBind.StartNode->BindChildNodeForce(BranchToBind.BranchRoot);
 	}
