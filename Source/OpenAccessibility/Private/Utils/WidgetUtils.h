f157f8ba30b1666fd9db37ba05742e3ab839d524
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2024-07-24 13:12:02 +0100

    Commented New Implementation
    Provided Comment Documentation for New Context Menu Implementation.

--- Source/OpenAccessibility/Private/Utils/WidgetUtils.h
+++ Source/OpenAccessibility/Private/Utils/WidgetUtils.h
@@ -4,6 +4,13 @@
 
 #include "CoreMinimal.h"
 
+/**
+ * Finds a Widget of the Target Type descending from the Search Root.
+ * @tparam T Target Widgets Type (Must be a Widget Type)
+ * @param SearchRoot The Widget to Perform the Search From.
+ * @param TargetWidgetType The Target Widgets Type, in String Format.
+ * @return A SharedPtr of a Found Target Descendant, on failure an invalid pointer is returned.
+ */
 template<class T>
 [[nodiscard]] FORCEINLINE TSharedPtr<T> GetWidgetDescendant(const TSharedRef<SWidget>& SearchRoot, FString TargetWidgetType)
 {
@@ -45,6 +52,14 @@ template<class T>
 	return TSharedPtr<T>();
 }
 
+
+/**
+ * Finds all widgets of the Target Type descending from the Search Root.
+ * @tparam T Target Widgets Type (Must be a Widget Type)
+ * @param SearchRoot The Widget To Perform the Search From.
+ * @param TargetWidgetType The Target Widgets Type, in String Format.
+ * @return An Array of Shared Pointers of the Target Widget Type.
+ */
 template <class T>
 [[nodiscard]] FORCEINLINE TArray<TSharedPtr<T>> GetWidgetDescendants(const TSharedRef<SWidget>& SearchRoot, FString TargetWidgetType)
 {
@@ -58,23 +73,19 @@ template <class T>
 		FoundDescendants.Add(StaticCastSharedRef<T>(SearchRoot));
 
 	{
-		TArray<FChildren*> ChildrenToSearch = TArray{
+		TArray<FChildren*> ChildrenToSearch = TArray {
 			SearchRoot->GetChildren()
 		};
 
-		FChildren* CurrentChildren;
-		TSharedPtr<SWidget> CurrentChild;
-		FString CurrentChildString;
-
 		while (ChildrenToSearch.Num() > 0)
 		{
-			CurrentChildren = ChildrenToSearch.Pop();
+			FChildren* CurrentChildren = ChildrenToSearch.Pop();
 
 			for (int i = 0; i < CurrentChildren->Num(); i++)
 			{
-				CurrentChild = CurrentChildren->GetChildAt(i);
+				TSharedPtr<SWidget> CurrentChild = CurrentChildren->GetChildAt(i);
 
-				CurrentChildString = CurrentChild->GetTypeAsString();
+				FString CurrentChildString = CurrentChild->GetTypeAsString();
 				CurrentChildString.RemoveSpacesInline();
 
 				if (CurrentChildString == TargetWidgetType)
@@ -88,6 +99,12 @@ template <class T>
 	return FoundDescendants;
 }
 
+/**
+ * Finds All Widget Slots Containing a Widget of a Target Type. Descending from the Search Root.
+ * @param SearchRoot The Starting Point For The Widget Search
+ * @param TargetTypes A Set Containing The Target Widget Types To Find.
+ * @return An Array of Widget Slots, Containing Widgets of a Target Type.
+ */
 [[nodiscard]] FORCEINLINE TArray<FSlotBase*> GetWidgetSlotsByType(const TSharedRef<SWidget>& SearchRoot, const TSet<FString>& TargetTypes)
 {
 	TArray<FSlotBase*> FoundDescendants = TArray<FSlotBase*>();
