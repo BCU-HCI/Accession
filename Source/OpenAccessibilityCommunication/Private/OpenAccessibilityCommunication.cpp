4d11956367a7b78a1426fd3bceaf337ee834b0fc
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2024-11-15 13:40:41 +0000

    Remapped Command Input Key to Alt
    Remapped the Command Input Key to Left Alt, to avoid conflicts with using the Spacebar with general Typing.

--- Source/OpenAccessibilityCommunication/Private/OpenAccessibilityCommunication.cpp
+++ Source/OpenAccessibilityCommunication/Private/OpenAccessibilityCommunication.cpp
@@ -95,18 +95,20 @@ bool FOpenAccessibilityCommunicationModule::Tick(const float DeltaTime)
 
 void FOpenAccessibilityCommunicationModule::HandleKeyDownEvent(const FKeyEvent& InKeyEvent)
 {
-	// If the Space Key is pressed, we will send a request to the Accessibility Server
-	if (InKeyEvent.GetKey() == EKeys::SpaceBar)
+	FKey TargetKey = EKeys::LeftAlt;
+
+	// If the Voice Command Key is Pressed, Toggle Audio Capture.
+	if (InKeyEvent.GetKey() == TargetKey && !InKeyEvent.IsRepeat())
 	{
-		if (InKeyEvent.IsShiftDown())
-		{
-			OA_LOG(LogOpenAccessibilityCom, Log, TEXT("AudioCapture Change"), TEXT("Stopping Audio Capture"));
-			AudioManager->StopCapturingAudio();
-		}
-		else
+		if (!AudioManager->IsCapturingAudio())
 		{
 			OA_LOG(LogOpenAccessibilityCom, Log, TEXT("AudioCapture Change"), TEXT("Starting Audio Capture"));
 			AudioManager->StartCapturingAudio();
+		}
+		else
+		{
+			OA_LOG(LogOpenAccessibilityCom, Log, TEXT("AudioCapture Change"), TEXT("Stopping Audio Capture"));
+			AudioManager->StopCapturingAudio();
 		}
 	}
 }
