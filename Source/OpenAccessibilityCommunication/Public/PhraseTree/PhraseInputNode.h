2556f501e0e737ae165b40b1ae145fe6ad3b088c
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2025-02-28 16:51:51 +0000

    Fixing Potential Linker Issues
    Fixing issue with templated classes having some issues with linking, depending on the Unreal Version and Build process.

--- Source/OpenAccessibilityCommunication/Public/PhraseTree/PhraseInputNode.h
+++ Source/OpenAccessibilityCommunication/Public/PhraseTree/PhraseInputNode.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "PhraseTree/PhraseNode.h"
+#include "PhraseTree/Utils.h"
+#include "OpenAccessibilityComLogging.h"
+
+#include "PhraseTree/Containers/Input/UParseIntInput.h"
 
 /**
  * 
@@ -12,21 +16,92 @@ template <typename InputType = int32>
 class OPENACCESSIBILITYCOMMUNICATION_API FPhraseInputNode : public FPhraseNode
 {
 public:
-	FPhraseInputNode(const TCHAR* InInputString);
-	FPhraseInputNode(const TCHAR* InInputString, TPhraseNodeArray InChildNodes);
-	FPhraseInputNode(const TCHAR* InInputString, TDelegate<void(FParseRecord& Record)> InOnPhraseParsed, TPhraseNodeArray InChildNodes);
-	FPhraseInputNode(const TCHAR* InInputString, TPhraseNodeArray InChildNodes, TDelegate<void (InputType Input)> InOnInputRecieved);
-	FPhraseInputNode(const TCHAR* InInputString, TDelegate<void(FParseRecord& Record)> InOnPhraseParsed, TPhraseNodeArray InChildNodes, TDelegate<void(InputType Input)> InOnInputRecieved);
+	FPhraseInputNode(const TCHAR* InInputString)
+		: FPhraseNode(InInputString)
+	{
+		
+	}
 
-	virtual ~FPhraseInputNode();
+	FPhraseInputNode(const TCHAR* InInputString, TPhraseNodeArray InChildNodes)
+		: FPhraseNode(InInputString, InChildNodes)
+	{
+		
+	}
+
+	FPhraseInputNode(const TCHAR* InInputString, TDelegate<void(FParseRecord& Record)> InOnPhraseParsed, TPhraseNodeArray InChildNodes)
+		: FPhraseNode(InInputString, InOnPhraseParsed, InChildNodes)
+	{
+
+	}
+
+	FPhraseInputNode(const TCHAR* InInputString, TPhraseNodeArray InChildNodes, TDelegate<void (InputType Input)> InOnInputRecieved)
+		: FPhraseNode(InInputString, InChildNodes)
+	{
+		OnInputReceived = InOnInputRecieved;
+	}
+
+	FPhraseInputNode(const TCHAR* InInputString, TDelegate<void(FParseRecord& Record)> InOnPhraseParsed, TPhraseNodeArray InChildNodes, TDelegate<void(InputType Input)> InOnInputRecieved)
+		: FPhraseNode(InInputString, InOnPhraseParsed, InChildNodes)
+	{
+		OnInputReceived = InOnInputRecieved;
+	}
+
+	virtual ~FPhraseInputNode() override
+	{
+		
+	}
 
 	// FPhraseNode Implementation
 	
-	virtual bool RequiresPhrase(const FString InPhrase) override;
+	virtual bool RequiresPhrase(const FString InPhrase) override
+	{
+		return MeetsInputRequirements(InPhrase);
+	}
 
-	virtual bool RequiresPhrase(const FString InPhrase, int32& OutDistance) override;
+	virtual bool RequiresPhrase(const FString InPhrase, int32& OutDistance) override
+	{
+		bool bMeetsRequirements = MeetsInputRequirements(InPhrase);
+		OutDistance = bMeetsRequirements ? 0 : INT32_MAX;
 
-	virtual FParseResult ParsePhrase(TArray<FString>& InPhraseArray, FParseRecord& InParseRecord) override;
+		return bMeetsRequirements;
+	}
+
+	virtual FParseResult ParsePhrase(TArray<FString>& InPhraseArray, FParseRecord& InParseRecord) override
+	{
+		if (InPhraseArray.Num() == 0)
+		{
+			UE_LOG(LogOpenAccessibilityCom, Log, TEXT("|| Emptied Phrase Array ||"))
+
+				return FParseResult(PHRASE_REQUIRES_MORE, AsShared());
+		}
+
+		if (MeetsInputRequirements(InPhraseArray.Last()))
+		{
+			// Get the Input String.
+			FString InputToRecord = InPhraseArray.Pop();
+
+			// Append the Input String to the Record.
+			InParseRecord.AddPhraseString(InputToRecord);
+
+			if (!InputToRecord.IsNumeric() && NumericParser::IsValidNumeric(InputToRecord, false))
+			{
+				NumericParser::StringToNumeric(InputToRecord, false);
+			}
+
+			if (!RecordInput(InputToRecord, InParseRecord))
+			{
+				UE_LOG(LogOpenAccessibilityCom, Log, TEXT("|| Unable to Record Input ||"))
+
+					return FParseResult(PHRASE_UNABLE_TO_PARSE, AsShared());
+			}
+
+			OnPhraseParsed.ExecuteIfBound(InParseRecord);
+
+			return ParseChildren(InPhraseArray, InParseRecord);
+		}
+
+		return FParseResult(PHRASE_UNABLE_TO_PARSE, AsShared());
+	}
 
 	// End FPhraseNode Implementation
 
@@ -40,7 +115,10 @@ protected:
 	/// </summary>
 	/// <param name="InPhrase">- The Phrase To Check If It Meets Requirements.</param>
 	/// <returns>True, if the Phrase Meets Requirements. Otherwise False.</returns>
-	virtual bool MeetsInputRequirements(const FString& InPhrase);
+	virtual bool MeetsInputRequirements(const FString& InPhrase)
+	{
+		return InPhrase.IsNumeric() || NumericParser::IsValidNumeric(InPhrase, false);
+	}
 
 	/// <summary>
 	/// Records the Input onto the Parse Record.
@@ -50,3 +128,24 @@ protected:
 	/// <returns>True, if the Input Was Successful in Recording. Otherwise False.</returns>
 	virtual bool RecordInput(const FString& InInput, FParseRecord& OutParseRecord);
 };
+
+
+template<typename InputType>
+FORCEINLINE bool FPhraseInputNode<InputType>::RecordInput(const FString& InInput, FParseRecord& OutParseRecord)
+{
+	return false;
+}
+
+FORCEINLINE bool FPhraseInputNode<int32>::RecordInput(const FString& InInput, FParseRecord& OutParseRecord)
+{
+	int32 Input = FCString::Atoi(*InInput);
+
+	UParseIntInput* ParseInput = MakeParseInput<UParseIntInput>();
+	ParseInput->SetValue(Input);
+
+	OutParseRecord.AddPhraseInput(BoundPhrase, ParseInput);
+
+	OnInputReceived.ExecuteIfBound(Input);
+
+	return true;
+}
